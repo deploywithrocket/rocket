ea1e04a458e5b6b359797e4c3310b5709a7cc436
Simon Rubuano <sr@mgk.dev>
2020-12-09 17:28:16 +0100

    ➕ Add `laravel/sanctum`

---
+++ database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personal_access_tokens', function (Blueprint $table) {
+            $table->id();
+            // Ulid version of $table->morphs('tokenable');
+            $table->string('tokenable_type');
+            $table->char('tokenable_id', 26);
+            $table->index(['tokenable_type', 'tokenable_id']);
+            $table->string('name');
+            $table->string('token', 64)->unique();
+            $table->text('abilities')->nullable();
+            $table->timestamp('last_used_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personal_access_tokens');
+    }
+}
