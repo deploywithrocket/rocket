7f93e9b0f4b8821dbb9b0b941a2a00775f67b5e1
Simon Rubuano <sr@mgk.dev>
2023-03-30 13:05:53 +0200

    docs: branding and latest tag

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,13 +1,32 @@
+import { resolve } from 'node:path'
+import { readFileSync } from 'node:fs'
 import { defineConfig } from 'vitepress'
 import Unocss from 'unocss/vite'
 
+const title = 'Rocket'
+const description = 'A free, open-source, and self-hosted alternative to services like Envoyer.'
+const url = 'https://deploywithrocket.dev'
+const image = '/og.jpg'
+const twitter = 'mgkprod'
+const github = 'https://github.com/deploywithrocket/core'
+
+const version = readFileSync(resolve('config/.version'), { encoding: 'utf-8' })
+
 export default defineConfig({
-    title: 'Rocket',
-    description: 'A free, open-source, and self-hosted alternative to services like Envoyer.',
+    title,
+    description,
 
     themeConfig: {
+        logo: '/logo.svg',
+
         nav: [
             { text: 'Guide', link: '/guide/', activeMatch: '/guide/' },
+            { text: `v${version}`, link: `${github}/releases/tag/${version}` },
+        ],
+
+        socialLinks: [
+            { icon: 'twitter', link: `https://twitter.com/${twitter}` },
+            { icon: 'github', link: `${github}` },
         ],
 
         sidebar: {
--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -1,2 +1,4 @@
-import 'uno.css'
 export { default } from 'vitepress/theme'
+
+import 'uno.css'
+import './styles/index.css'
